c5ba5f14c9e14d50d0c853dc0d8479e1e1dd1f25
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-02-25 15:53:38 +0100

    (conf) Finish deploy
    App has been deployed successful

--- FitWin/FitWin/settings.py
+++ FitWin/FitWin/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    DEBUG= True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
