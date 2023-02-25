d5034d95891d0a8ec2d3c00d6fb66e25db6c4b25
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-02-25 14:20:24 +0100

    (fix) Deploy fix
    Enable debug to see what is happening on deploy

--- FitWin/FitWin/settings.py
+++ FitWin/FitWin/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    DEBUG= True
 
 
 # Default primary key field type
