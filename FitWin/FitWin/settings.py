107c9453e38975b6d799b87e25b01c69d8210808
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-02-25 15:47:32 +0100

    (conf) Updated settings to deploy
    Updated settings to deploy

--- FitWin/FitWin/settings.py
+++ FitWin/FitWin/settings.py
@@ -126,12 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     DEBUG= True
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
