ffe1c893907a93d40cfca27884bb22c12fa66039
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-02-25 16:24:18 +0100

    (fix) Deploy
    Trying to fix deploy

--- FitWin/FitWin/settings.py
+++ FitWin/FitWin/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
