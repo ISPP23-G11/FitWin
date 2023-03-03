9a27722659929d93d15b961d65e822029e05fa8a
Borjapc417 <borjacalerap@gmail.com>
2023-03-03 18:50:58 +0100

    (feat): Added book announcement
    Now yopu can book an announcement with an HTTP GET request to an URL

--- FitWin/anuncios/urls.py
+++ FitWin/anuncios/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('announcements/list-own/all', views.list_own_all, name='list_own_all'),
     path('announcements/add-categories/<int:announcement_id>', views.add_categories, name='add_categories'),
     path('announcements/delete-categories/<int:announcement_id>/<int:category_id>', views.delete_categories, name='delete_categories'),
+    path('announcements/book/<int:announcement_id>', views.book_announcement, name='book'),
 ]
