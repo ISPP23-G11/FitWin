b21a52d5c9da34f6da825bce2ace4879afa3f556
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-03-05 12:03:09 +0100

    (feat): added menu
    Added responsive menu with icons and principal buttons

--- FitWin/FitWin/settings.py
+++ FitWin/FitWin/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'landingPage',
+    'bootstrap5',
+    'jquery',
 ]
 
 MIDDLEWARE = [
--- FitWin/landingPage/views.py
+++ FitWin/landingPage/views.py
@@ -6,3 +6,6 @@ from django.template import loader
 def home(request):
     template = loader.get_template('landingPage/home.html')
     return HttpResponse(template.render())
+def menu(request):
+    template = loader.get_template('menu.html')
+    return HttpResponse(template.render())
