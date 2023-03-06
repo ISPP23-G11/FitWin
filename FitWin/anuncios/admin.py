51b24b0a0cf1009a8a3fa7c7f77dac2316bf60b0
Raul Gallardo <raugalroc@alum.us.es>
2023-03-06 19:26:13 +0100

    (feat): Data entered by default
    Created data.json file to load default data to database and set parameters blank=True to attributes clients and categories.

--- FitWin/anuncios/admin.py
+++ FitWin/anuncios/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Trainer)
+admin.site.register(Client)
 admin.site.register(Announcement)
 admin.site.register(Category)
+
