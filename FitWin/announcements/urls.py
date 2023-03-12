7d0eaafd8ef3499f7c5741e72f056aa2e8b00ddc
Eduardo <educielan@alum.us.es>
2023-03-12 20:34:22 +0100

    eliminarAnuncio-issue#9

--- FitWin/announcements/urls.py
+++ FitWin/announcements/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('announcements/delete-categories/<int:announcement_id>/<int:category_id>', views.delete_categories, name='delete_categories'),
     path('announcements/book/<int:announcement_id>', views.book_announcement, name='book'),
     path('announcements/cancelBook/<int:announcement_id>', views.cancel_book_announcement, name='cancel_book'),
+    path('announcements/delete-announce/<int:announcement_id>', views.delete_announce, name='delete_announce'),
 ]
