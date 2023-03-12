e316ef08b8caf98949f80eefa708b27150d4381f
ignjorde <ignjorde@alum.us.es>
2023-03-12 17:11:03 +0100

    (feat): Add Calendar model

--- FitWin/announcements/models.py
+++ FitWin/announcements/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Trainer, Client
+from users.models import Trainer, Client, User
 
 class Category(models.Model):
     name=models.CharField(max_length=250, verbose_name='Categoria')
@@ -11,11 +11,11 @@ class Announcement(models.Model):
     title = models.CharField(max_length=250, verbose_name='Titulo')
     description = models.TextField(verbose_name='Descripcion')
     place = models.CharField(max_length=250, verbose_name='Lugar')
-    price=models.FloatField()
-    capacity=models.IntegerField()
-    trainer=models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    clients=models.ManyToManyField(Client, blank=True)
-    categories=models.ManyToManyField(Category, blank=True)
+    price = models.FloatField()
+    capacity = models.IntegerField()
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    clients = models.ManyToManyField(Client, blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
     start_date = models.DateTimeField(auto_now_add=False, verbose_name='Fecha de inicio')
     finish_date = models.DateTimeField(auto_now_add=False, verbose_name='Fecha de fin')
     invitation_sent = models.BooleanField(default=False)
@@ -28,4 +28,6 @@ class Announcement(models.Model):
     def __str__(self):
         return self.title
     
-
+class Calendar(models.Model):
+    google_calendar_id = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
