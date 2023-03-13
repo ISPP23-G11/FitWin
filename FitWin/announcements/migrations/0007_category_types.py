7f3518172595789a4a2b50395397b45763f7f4d7
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2023-03-13 23:23:40 +0100

    Update 0007_category_types.py

--- FitWin/announcements/migrations/0007_category_types.py
+++ FitWin/announcements/migrations/0007_category_types.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('announcements', '0003_alter_category_name'),
+        ('announcements', '0001_initial'),
     ]
 
     operations = [
