07ef59998d9986be807e14b11a1dc00bede280ac
Raul Gallardo <raugalroc@alum.us.es>
2023-03-16 20:10:40 +0100

    (feat): Started user register
    Started user register for trainers and for clients

--- FitWin/authentication/views.py
+++ FitWin/authentication/views.py
@@ -5,6 +5,11 @@ from django.contrib import messages
 from django.shortcuts import HttpResponse, redirect
 from django.template import loader
 from users.models import Trainer, Client 
+from django.views.generic import View
+from .forms import SignUpForm
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 def login(request):
     if request.method == 'POST':
@@ -24,3 +29,67 @@ def login(request):
         template = loader.get_template("account/login.html") 
         context = {}
         return HttpResponse(template.render(context, request))
+
+
+        
+
+def trainer_register(request):
+     if request.method == 'POST':
+          form = SignUpForm(request.POST)
+
+          if form.is_valid():
+               
+               post_save.connect(create_trainer_profile, sender=User)
+               post_save.connect(save_trainer_profile, sender=User)
+               
+               form.save()
+
+               username = form.cleaned_data['username']
+               messages.success(request, f'Usuario {username} creado')
+               return redirect('/')
+     else:
+          form=SignUpForm()
+
+     context = { 'form' : form }
+     return render(request, 'account/trainerRegister.html', context)
+
+def client_register(request):
+     if request.method == 'POST':
+          form = SignUpForm(request.POST)
+
+          if form.is_valid():
+               
+               post_save.connect(create_client_profile, sender=User)
+               post_save.connect(save_client_profile, sender=User)
+               
+               form.save()
+   
+               username = form.cleaned_data['username']
+               messages.success(request, f'Usuario {username} creado')
+               return redirect('/')
+     else:
+          form=SignUpForm()
+
+     context = { 'form' : form }
+     return render(request, 'account/clientRegister.html', context)
+        
+
+        
+def create_client_profile(sender, instance, created, **kwargs):
+    if created:
+        Client.objects.create(user=instance)
+
+
+def save_client_profile(sender, instance, **kwargs):
+    instance.client.save()
+
+
+def create_trainer_profile(sender, instance, created, **kwargs):
+    if created:
+        Trainer.objects.create(user=instance)
+
+def save_trainer_profile(sender, instance, **kwargs):
+    instance.trainer.save()
+
+
+
--- FitWin/users/models.py
+++ FitWin/users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Trainer(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,9 +12,12 @@ class Trainer(models.Model):
     bio = models.TextField(max_length=150, null=True, blank=True)
 
 
+
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture=models.ImageField(blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
     birthday=models.DateField(null=True, blank=True)
     bio = models.TextField(max_length=150, null=True, blank=True)
+    
+
