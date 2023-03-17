61c258ef4c46c86a0a7192b3356d935990ad7040
borpincal <borpincal@alum.us.es>
2023-03-17 16:56:28 +0100

    (feat): Created register users
    Now you can register as trainer or client

--- FitWin/authentication/views.py
+++ FitWin/authentication/views.py
@@ -10,6 +10,7 @@ from .forms import SignUpForm
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from datetime import datetime
 
 def login(request):
     if request.method == 'POST':
@@ -25,71 +26,104 @@ def login(request):
                 return redirect('/trainers')
             elif client:
                 return redirect('/clients')
+        else:
+            messages.error(request, "El usuario y la contraseña son incorrectos")
+            return redirect("/login")
     else:
         template = loader.get_template("account/login.html") 
         context = {}
         return HttpResponse(template.render(context, request))
 
 
-        
-
 def trainer_register(request):
-     if request.method == 'POST':
-          form = SignUpForm(request.POST)
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        password_again = request.POST.get("password_again", "")
+        bio = request.POST.get("bio", "")
+        birthday = request.POST.get("birthday", "")
+        picture = request.FILES["picture"]
+        name = request.POST.get("name", "")
+        last_name = request.POST.get("last_name", "")
 
-          if form.is_valid():
-               
-               post_save.connect(create_trainer_profile, sender=User)
-               post_save.connect(save_trainer_profile, sender=User)
-               
-               form.save()
+        old_user = User.objects.filter(username = username)
 
-               username = form.cleaned_data['username']
-               messages.success(request, f'Usuario {username} creado')
-               return redirect('/')
-     else:
-          form=SignUpForm()
+        errors = False
+        if old_user:
+            errors = True
+            messages.error(request, "Este nombre de usuario ya está cogido")
 
-     context = { 'form' : form }
-     return render(request, 'account/trainerRegister.html', context)
+        if password != password_again:
+            errors = True
+            messages.error(request, "Las contraseñas no coinciden")
+
+        if birthday!= "":
+            birthday = datetime.strptime(birthday, '%Y-%m-%d')
+            if birthday >= datetime.now():
+                errors = True
+                messages.error(request, "La fecha del cumpleaños tiene que ser anterior a hoy")
+        else:
+            errors = True
+            messages.error(request, "La fecha de naciemiento es obligatoria")
+
+        if not errors:
+            user = User.objects.create_user(username = username, password=password, email=email, first_name=name, last_name=last_name)
+            user.save()
+            trainer = Trainer(user = user, bio=bio, birthday=birthday)
+            trainer.picture.save(picture.name, picture)
+            trainer.save()
+            return redirect("/login")
+        else:
+            return redirect("/trainerRegister/")
+    else:
+        template = loader.get_template("account/trainerRegister.html") 
+        context = {}
+        return HttpResponse(template.render(context, request))
 
 def client_register(request):
-     if request.method == 'POST':
-          form = SignUpForm(request.POST)
-
-          if form.is_valid():
-               
-               post_save.connect(create_client_profile, sender=User)
-               post_save.connect(save_client_profile, sender=User)
-               
-               form.save()
-   
-               username = form.cleaned_data['username']
-               messages.success(request, f'Usuario {username} creado')
-               return redirect('/')
-     else:
-          form=SignUpForm()
-
-     context = { 'form' : form }
-     return render(request, 'account/clientRegister.html', context)
-        
-
-        
-def create_client_profile(sender, instance, created, **kwargs):
-    if created:
-        Client.objects.create(user=instance)
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        password_again = request.POST.get("password_again", "")
+        bio = request.POST.get("bio", "")
+        birthday = request.POST.get("birthday", "")
+        picture = request.FILES["picture"]
+        name = request.POST.get("name", "")
+        last_name = request.POST.get("last_name", "")
 
 
-def save_client_profile(sender, instance, **kwargs):
-    instance.client.save()
+        old_user = User.objects.filter(username = username)
 
+        errors = False
+        if old_user:
+            errors = True
+            messages.error(request, "Este nombre de usuario ya está cogido")
 
-def create_trainer_profile(sender, instance, created, **kwargs):
-    if created:
-        Trainer.objects.create(user=instance)
+        if password != password_again:
+            errors = True
+            messages.error(request, "Las contraseñas no coinciden")
 
-def save_trainer_profile(sender, instance, **kwargs):
-    instance.trainer.save()
+        if birthday!= "":
+            birthday = datetime.strptime(birthday, '%Y-%m-%d')
+            if birthday >= datetime.now():
+                errors = True
+                messages.error(request, "La fecha del cumpleaños tiene que ser anterior a hoy")
+        else:
+            errors = True
+            messages.error(request, "La fecha de naciemiento es obligatoria")
 
-
-
+        if not errors:
+            user = User.objects.create_user(username = username, password=password, email=email, first_name=name, last_name=last_name)
+            user.save()
+            client = Client(user = user, bio=bio, birthday=birthday)
+            client.picture.save(picture.name, picture)
+            client.save()
+            return redirect("/login")
+        else:
+            return redirect("/clientRegister/")
+    else:
+        template = loader.get_template("account/clientRegister.html") 
+        context = {}
+        return HttpResponse(template.render(context, request))
