2696576f43bb2832afa7b705990b1c90f02420e7
borpincal <borpincal@alum.us.es>
2023-03-19 19:03:16 +0100

    (feat): View connected with backend
    View has been connected with backend

--- FitWin/users/urls.py
+++ FitWin/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('trainers', views.handler_trainers, name='main'),
     path('clients', views.handler_clients, name='main'),
-    path('client_details', views.handler_client_details, name='main'),
+    path('client/<int:client_id>', views.handler_client_details, name='main'),
 
 
 ]
--- FitWin/users/views.py
+++ FitWin/users/views.py
@@ -33,7 +33,12 @@ def handler_clients(request):
         return HttpResponse(template.render(context, request))
 
 @login_required
-def handler_client_details(request):
-    context = {}
+def handler_client_details(request, client_id):
+    client = Client.objects.filter(id = client_id)
+    if client:
+        client = client.get()
+    else:
+        messages.error(request, "No se ha encontrado al cliente")
+    context = {'client':client}
     template = loader.get_template("client_details.html") 
     return HttpResponse(template.render(context, request))
