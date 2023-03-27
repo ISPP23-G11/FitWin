0c6a833cfff509aa5d79ebf07b357d53609a57bc
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-03-27 22:32:33 +0200

    (conf) Update build.sh
    Updated build.sh to deploy

--- FitWin/build.sh
+++ FitWin/build.sh
@@ -4,3 +4,5 @@ pip install -r ../requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@fitwin.com', '12complexpassword34')" | python manage.py shell
