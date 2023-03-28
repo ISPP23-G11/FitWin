d139fa7342e05b07d22d8b6da11b90f1233b0287
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-03-28 11:44:52 +0200

    (conf) Update build.sh
    Updated build.sh

--- FitWin/build.sh
+++ FitWin/build.sh
@@ -3,5 +3,12 @@ set -o errexit
 pip install -r ../requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations announcements
+python manage.py makemigrations authentication
+python manage.py makemigrations chat
+python manage.py makemigrations landingPage
+python manage.py makemigrations payment
+python manage.py makemigrations searching
+python manage.py makemigrations users
 python manage.py makemigrations
 python manage.py migrate
