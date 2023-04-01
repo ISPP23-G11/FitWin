ac9f337afdf190572c0f6f008bdf01ac73a027b9
Raul Gallardo <raugalroc@alum.us.es>
2023-04-01 19:22:50 +0200

    (feat): Integrated payment gateway
    Integrated payment gateway with subscription update system

--- FitWin/payments/urls.py
+++ FitWin/payments/urls.py
@@ -5,9 +5,6 @@ from .import views
 app_name="payments"
 
 urlpatterns = [
-
-
-        path('', views.home, name='home'),
         path('create-checkout-session/', views.create_checkout_session, name='checkout'),
         path('success/', views.success,name='success'),
         path('cancel/', views.cancel,name='cancel'),
--- FitWin/payments/views.py
+++ FitWin/payments/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import stripe
 from django.utils.html import strip_tags
 from django.http import JsonResponse
+from users.views import upgrade_suscription, is_premium
+from users.models import Trainer
+from django.contrib import messages
+
 
 # Create your views here.
-
-def home(request):
-
-    url = '/payments/create-checkout-session/'
-    context = {'url':url}
-    return render(request,'payments/payment.html', context=context)
-
 
 def cancel(request):
     return render(request,'payments/cancel.html')
 
 def success(request):
+    user = request.user
+    trainer = Trainer.objects.get(user = user)
+    upgrade_suscription(trainer)
     return render(request,'payments/success.html')
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -28,24 +29,34 @@ def create_checkout_session(request):
     print("Entra en checkout")
    
     total = 4.99
-   
-      
+     
     total_formated = str("%.2f" % total).replace('.','')
+
+    user = request.user
+    trainer = Trainer.objects.get(user = user)
+
+    if not is_premium(trainer):
+
+        session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'eur',
+                'product_data': {
+                    'name': 'Pago en Fitwin',
+                },
+                'unit_amount': total_formated,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=YOUR_DOMAIN + '/payments/success',
+        cancel_url=YOUR_DOMAIN + '/payments/cancel',
+        )
+        
+
+        return JsonResponse({'id': session.id})
     
-    session = stripe.checkout.Session.create(
-    payment_method_types=['card'],
-    line_items=[{
-        'price_data': {
-            'currency': 'eur',
-            'product_data': {
-                'name': 'Pago en Fitwin',
-             },
-             'unit_amount': total_formated,
-         },
-         'quantity': 1,
-     }],
-     mode='payment',
-     success_url=YOUR_DOMAIN + '/payments/success',
-     cancel_url=YOUR_DOMAIN + '/payments/cancel',
-    )
-    return JsonResponse({'id': session.id})
+    else:
+        messages.error(request, "Ya eres entrenador premium")
+        return redirect('/trainers')
