6c58538cc7d0053e84e358f5ab98baa8261af34c
Pablo Ramos <99967600+pabramber@users.noreply.github.com>
2023-05-06 12:22:16 +0200

    (conf) Update url

--- FitWin/FitWin/urls.py
+++ FitWin/FitWin/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +30,7 @@ urlpatterns = [
     path('',include('recommendations.urls')),
     path('chat/', include('chat.urls')),
     path('payments/', include('payments.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
